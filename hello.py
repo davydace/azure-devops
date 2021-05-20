511d716738537569d2f1e3626aa48c1c444d2547
David Edokpayid <david.edokpayi@outlook.com>
2021-05-20 22:03:30 +0100

    successful run of hello.py script

---
+++ hello.py
@@ -0,0 +1,15 @@
+import click
+#from click.termui import prompt
+
+@click.command(help="This is just a hello app. It does nothing")
+@click.option("--name", prompt="I need your name", help="Need name")
+@click.option("--color", prompt="I need your color", help="This is your color")
+def hello(name, color):
+    if name == "Thor":
+        click.echo("Thor your are always red")
+        click.echo(click.style(f"Hello World! {name}", fg="red"))
+    else:
+        pass
+
+if __name__ == "__main__":
+        hello()
